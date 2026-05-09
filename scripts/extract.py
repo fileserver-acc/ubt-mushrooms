c3a7493aebc05e0b9bc944490379f9de1487d523
fileserver-acc <shlockfish@gmail.com>
2026-05-09 04:15:26 +0200

    Update extract.py

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,7 +1,6 @@
 import os
 import fitz
 from tqdm import tqdm
-from extract_images import extract
 import sys
 from PIL import Image
 
@@ -26,17 +25,15 @@ def extract(source: str, destination: str):
                 image_file.write(image_bytes)
 
 
-if __name__ == "__main__":
-    in_path = sys.argv[1]
-    outdir = sys.argv[2]
+in_path = sys.argv[1]
+outdir = sys.argv[2]
 
-    print(f"extracting from {in_path}...")
-    doc = fitz.Document(in_path)
+print(f"extracting from {in_path}...")
+doc = fitz.Document(in_path)
 
-    if not (os.path.exists(os.path.join(outdir, in_path.split("/").pop().split(".")[0])) and os.path.isdir(os.path.join(outdir, in_path.split("/").pop().split(".")[0]))):
-        os.makedirs(os.path.join(outdir, in_path.split("/").pop().split(".")[0]), exist_ok=True)
+os.makedirs(outdir)
 
-    try:
-        extract(in_path, os.path.join(outdir, in_path.split("/").pop().split(".")[0]))
-    except Exception as e:
-        print(f"error extracting file {in_path}: {e}", file=sys.stderr)
+try:
+    extract(in_path, outdir)
+except Exception as e:
+    print(f"error extracting file {in_path}: {e}", file=sys.stderr)
